c4003c9f9cf03fa9f3f18a9ed7bffa06e56b0841
Michael Keenan <keenan@michaelkeenan.com>
2022-05-14 05:53:47 -0400

    Update ubuntuSetup.sh

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -5,7 +5,7 @@
 
 sudo apt-get update && sudo apt-get -y upgrade
 
-sudo apt-get install -y net-tools build-essential curl wget mlocate git gnupg nano tcpdump python3 python3-dev python3-pip libssl-dev libffi-dev
+sudo apt-get install -y net-tools build-essential curl wget mlocate git gnupg nano tcpdump python3 python3-dev python3-pip libssl-dev libffi-dev open-vm-tools
 
 sudo cp ~/.bashrc ~/.bashrc.original
 
