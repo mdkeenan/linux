01743953eaf3c7f31b4c87e8d013b8485fc0cc3a
Michael Keenan <keenan@michaelkeenan.com>
2022-05-14 16:14:40 -0400

    Update ubuntuSetup.sh

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -44,7 +44,7 @@ CRONY="/etc/crontab"
 if grep -q mycron "$CRONY"; then
     :
 else
-    echo "NOTE: ~/mycron.sh is not amended to crontab. Amending."
+    echo "NOTE: /usr/local/src/mycron.sh is not amended to crontab. Amending."
     sudo echo "32 4 * * 0 /usr/local/src/mycron.sh" >> /etc/crontab
 fi
 
