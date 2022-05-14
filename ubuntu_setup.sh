b2e8dad46a35e7af88e4a20a2cabe2b0fb77900f
Michael Keenan <keenan@michaelkeenan.com>
2022-05-14 00:31:51 -0400

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2,7 +2,7 @@
 
 # source <(curl -s https://raw.githubusercontent.com/mdkeenan/linux/master/ubuntu_setup.sh)
 
-sudo apt-get update -y && apt-get upgrade -y
+sudo apt-get update && sudo apt-get -y upgrade
 
 sudo apt-get install -y net-tools build-essential curl wget mlocate git gnupg nano tcpdump python3 python3-dev python3-pip libssl-dev libffi-dev
 
