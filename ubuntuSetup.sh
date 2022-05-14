8c5baf0e114d5308e5cc42367ad79f893cacfa94
Michael Keenan <keenan@michaelkeenan.com>
2022-05-14 14:22:11 -0400

    Update ubuntuSetup.sh

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -7,20 +7,26 @@ sudo apt-get update && sudo apt-get -y upgrade
 
 sudo apt-get install -y net-tools build-essential curl wget mlocate git gnupg nano tcpdump python3 python3-dev python3-pip libssl-dev libffi-dev open-vm-tools
 
-sudo cp ~/.bashrc ~/.bashrc.original
+ORIG=~/.bashrc.original
+
+if test -f "$ORIG"; then
+    :
+else
+    cp ~/.bashrc ~/.bashrc.original
+fi
 
 sudo wget -q https://raw.githubusercontent.com/mdkeenan/linux/master/bashrc -O ~/.bashrc
 
-sudo wget -q https://raw.githubusercontent.com/mdkeenan/linux/master/mycron.sh -O ~/.mycron.sh
+sudo wget -q https://raw.githubusercontent.com/mdkeenan/linux/master/mycron.sh -O ~/mycron.sh
 
-sudo chmod +x ~/.mycron.sh
+sudo chmod +x ~/mycron.sh
 
-File="/etc/crontab"
+CRONY="/etc/crontab"
 
-if grep -q mycron "$File"; then
+if grep -q mycron "$CRONY"; then
     :
 else
-    echo "32 4 * * 0 ~/.mycron.sh" >> /etc/crontab
+    sudo echo "32 4 * * 0 ~/mycron.sh" >> /etc/crontab
 fi
 
 source ~/.bashrc
