776318b1dad1350d9de0dfc30ecc79aef0bce4cc
Michael Keenan <keenan@michaelkeenan.com>
2022-05-14 05:46:40 -0400

    Update ubuntuSetup.sh

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -7,6 +7,8 @@ sudo apt-get update && sudo apt-get -y upgrade
 
 sudo apt-get install -y net-tools build-essential curl wget mlocate git gnupg nano tcpdump python3 python3-dev python3-pip libssl-dev libffi-dev
 
+sudo cp ~/.bashrc ~/.bashrc.original
+
 wget -q https://raw.githubusercontent.com/mdkeenan/linux/master/bashrc -O ~/.bashrc
 
 sudo cp ~/.bashrc /root/.bashrc
