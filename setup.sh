8dbf791e14ba30eedb0cfcbb0a26dce3ca42df96
Michael Keenan <keenan@michaelkeenan.com>
2022-05-14 22:43:04 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the following commented command to set up Ubuntu.
-# sudo apt-get install curl && source <(curl -k -s https://raw.githubusercontent.com/mdkeenan/linux/master/ubuntuSetup.sh)
+# sudo apt-get install curl && source <(curl -k -s https://raw.githubusercontent.com/mdkeenan/linux/master/setup.sh)
 
 # Update and upgrade the system.
 sudo apt-get update && sudo apt-get upgrade -y -qq > /dev/null
