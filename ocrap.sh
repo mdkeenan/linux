fb91acff4dd2c216f238105c0d986dd746fdb72b
Michael Keenan <keenan@michaelkeenan.com>
2022-05-14 21:05:58 -0400

    Update ocrap.sh

--- ocrap.sh
+++ ocrap.sh
@@ -3,7 +3,9 @@
 # This will update from https://raw.githubusercontent.com/mdkeenan/linux/master/ocrap.sh every hour.
 # /etc/crontab 0 * * * * root sh /usr/local/src/ocrap.sh
 
-wget --no-check-certificate -q https://raw.githubusercontent.com/mdkeenan/linux/master/ocrap.sh -O /usr/local/src/ocrap.varsource
+# wget --no-check-certificate -q https://raw.githubusercontent.com/mdkeenan/linux/master/ocrap.sh -O /usr/local/src/ocrap.varsource
+
+curl -k -s https://raw.githubusercontent.com/mdkeenan/linux/master/ocrap.sh -o /usr/local/src/ocrap.varsource
 
 cp /usr/local/src/ocrap.sh /usr/local/src/ocrap.varlocal
 
@@ -16,8 +18,8 @@ OCRAPSUMLOCAL2=$(echo $OCRAPSUMLOCAL | awk -F" " '{print $1}')
 echo $OCRAPSUMSOURCE2
 echo $OCRAPSUMLOCAL2
 
-now=$(date +"%T")
-echo "Updated: $now" >> /usr/local/src/ocrap
+# now=$(date +"%T")
+# echo "Updated: $now" >> /usr/local/src/ocrap
 
-rm /usr/local/src/ocrap.varsource
-rm /usr/local/src/ocrap.varlocal
+# rm /usr/local/src/ocrap.varsource
+# rm /usr/local/src/ocrap.varlocal
