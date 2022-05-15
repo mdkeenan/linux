c4f4fd7b3829691fddc7f73c7e6a24f3fb23660d
Michael Keenan <keenan@michaelkeenan.com>
2022-05-15 18:10:45 -0400

    Update setup-commands.sh

--- setup-commands.sh
+++ setup-commands.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Run the following commented command to set up Ubuntu.
-# sudo apt-get install -qqym curl && source <(curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/setup.sh)
+# Run the following commented command AS ROOT to set up the system.
+# apt-get install -qqym curl && source <(curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/setup.sh)
 
 # Set apt-get to non-interactive.
 echo "Setting DEBIAN_FRONTEND to noninteractive".
@@ -9,14 +9,14 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo "Starting apt-get update and apt-get upgrade."
 # Update and upgrade the system.
-sudo apt-get update -y
-sudo apt-get upgrade -y
+apt-get update -y
+apt-get upgrade -y
 
 echo "Finished apt-get update and apt-get upgrade."
 
 echo "Starting apt-get install common tools."
 # Install my commonly used packages.
-sudo apt-get install -ym net-tools openssh-server build-essential curl wget mlocate git gnupg nano tcpdump python3 python3-dev python3-pip libssl-dev libffi-dev open-vm-tools
+apt-get install -ym net-tools openssh-server build-essential curl wget mlocate git gnupg nano tcpdump python3 python3-dev python3-pip libssl-dev libffi-dev open-vm-tools
 
 echo "Finished apt-get install common tools."
 
@@ -30,45 +30,43 @@ if  test "$USER" = "root"; then
         :
     else
         echo "NOTE: $ROOTRCCOPY does not exist. Making copy of bashrc."
-        sudo cp /root/.bashrc /root/.bashrc.original
+        cp /root/.bashrc /root/.bashrc.original
     fi
 else
     if test -f "$USERRCCOPY"; then
         :
     else
         echo "NOTE: $USERRCCOPY does not exist. Making copy of bashrc."
-        sudo cp /home/$USER/.bashrc /home/$USER/.bashrc.original
+        cp /home/$USER/.bashrc /home/$USER/.bashrc.original
     fi
 fi
 
 echo "Downloading bashrc file and setting it as ~/.bashrc"
 
 # Download and replace bashrc file for current user.
-sudo curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/bashrc -o ~/.bashrc
-# sudo wget -q --no-check-certificate https://raw.githubusercontent.com/mdkeenan/linux/master/bashrc -O ~/.bashrc
+curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/bashrc -o ~/.bashrc
 
 echo "Done donwloading bashrc file and setting it as ~/.bashrc"
 
 echo "Downloading mycron.sh and setting it as /usr/local/src/mycron.sh"
 
 # Download mycron.sh file that contains a list of regularly scheduled commands.
-sudo curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/mycron.sh -o /usr/local/src/mycron.sh
-# sudo wget -q --no-check-certificate https://raw.githubusercontent.com/mdkeenan/linux/master/mycron.sh -O /usr/local/src/mycron.sh
+curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/mycron.sh -o /usr/local/src/mycron.sh
 
 echo "Done downloading mycron.sh and setting it as /usr/local/src/mycron.sh"
 
 echo "Downloading ocrap.sh file and setting it as /usr/local/src/ocrap.sh"
 
 # Download ocrap.sh file that contains a list of commands that are ran every hour.
-sudo curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/ocrap.sh -o /usr/local/src/ocrap.sh
+curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/ocrap.sh -o /usr/local/src/ocrap.sh
 
 echo "Done downloading ocrap.sh file and setting it as /usr/local/src/ocrap.sh"
 
 echo "Making mycron.sh and ocrap.sh executable."
 
 # Make mycron.sh and ocrap.sh executable.
-sudo chmod +x /usr/local/src/mycron.sh
-sudo chmod +x /usr/local/src/ocrap.sh
+chmod +x /usr/local/src/mycron.sh
+chmod +x /usr/local/src/ocrap.sh
 
 echo "Done making mycron.sh and ocrap.sh executable."
 
@@ -80,7 +78,7 @@ if grep -q mycron "$CRONY"; then
     :
 else
     echo "NOTE: /usr/local/src/mycron.sh is not amended to crontab. Amending."
-    sudo echo "32 4 * * 0 root sh /usr/local/src/mycron.sh" >> /etc/crontab
+    echo "32 4 * * 0 root sh /usr/local/src/mycron.sh" >> /etc/crontab
 fi
 
 echo "Done adding mycron.sh to crontab."
@@ -91,7 +89,7 @@ if grep -q ocrap "$CRONY"; then
     :
 else
     echo "NOTE: /usr/local/src/ocrap.sh is not amended to crontab. Amending."
-    sudo echo "0 * * * * root sh /usr/local/src/ocrap.sh" >> /etc/crontab
+    echo "0 * * * * root sh /usr/local/src/ocrap.sh" >> /etc/crontab
 fi
 
 echo "Done adding ocrap.sh to crontab."
