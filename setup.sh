a0247c730dbe2525407dcbae63aa261446289a55
Michael Keenan <keenan@michaelkeenan.com>
2022-05-15 17:26:41 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,8 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo "Starting apt-get update and apt-get upgrade."
 # Update and upgrade the system.
-sudo apt-get update && sudo apt-get upgrade -y -qq > /dev/null
+apt-get update -y
+apt-get upgrade -y
 
 echo "Finished apt-get update and apt-get upgrade."
 
