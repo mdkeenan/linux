c044280738f8c75b867c35bd3c94e89b096cd4b5
Michael Keenan <keenan@michaelkeenan.com>
2022-05-16 18:36:42 -0400

    Update ocrap.sh

--- ocrap.sh
+++ ocrap.sh
@@ -8,9 +8,3 @@ curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/ocrap.sh -o 
 
 # Download latest version of ocrap-commands.sh.
 curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/ocrap-commands.sh | source
-
-# Replace old version of ocrap.sh with new version.
-mv /usr/local/src/ocraptmp.sh /usr/local/src/ocrap.sh
-
-# Delete ocraptmp.sh
-rm /usr/local/src/ocraptmp.sh
