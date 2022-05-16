572d8f6b5f6eae1fb5ad2f79a7c082e1f79d077d
Michael Keenan <keenan@michaelkeenan.com>
2022-05-16 18:55:33 -0400

    Update mycron.sh

--- mycron.sh
+++ mycron.sh
@@ -3,6 +3,11 @@
 # This will update from https://raw.githubusercontent.com/mdkeenan/linux/master/mycron.sh every Sundary at 04:32 and run the commands.
 # 32 4 * * 0 root sh /usr/local/src/mycron.sh >> /etc/crontab
 
-sudo apt-get update && sudo apt-get upgrade -y -qq > /dev/null
+# Download current version of ocrap.sh and save it to ocraptmp.sh
+curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/mycron.sh -o /usr/local/src/mycrontmp.sh
 
-curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/mycron.sh -o /usr/local/src/mycron.sh
+# Download latest version of ocrap-commands.sh.
+curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/mycron-commands.sh | sudo bash
+
+# Replace old version of ocrap.sh with new version.
+mv /usr/local/src/mycrontmp.sh /usr/local/src/mycron.sh
