71473b763ae66494734a9cbff5a267ad72e9c270
Michael Keenan <keenan@michaelkeenan.com>
2022-05-16 16:02:18 -0400

    Create ocrap-commands.sh

---
+++ ocrap-commands.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This will be ran by https://raw.githubusercontent.com/mdkeenan/linux/master/ocrap.sh every hour on applicable machines.
+# Can also be ran with: source <(curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/ocrap-commands.sh)
+
+# Log the date and time the command is ran to the ocraplog.log.
+now=$(date)
+echo "ocrap.sh ran at $now" >> /usr/local/src/ocraplog.log
