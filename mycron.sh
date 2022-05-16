20d260d159e6605fd5d227aec2520bd2b251d9d6
Michael Keenan <keenan@michaelkeenan.com>
2022-05-15 22:44:33 -0400

    Update mycron.sh

--- mycron.sh
+++ mycron.sh
@@ -3,11 +3,11 @@
 # This will update from https://raw.githubusercontent.com/mdkeenan/linux/master/mycron.sh every Sundary at 04:32 and run the commands.
 # 32 4 * * 0 root sh /usr/local/src/mycron.sh >> /etc/crontab
 
-CRONY="/etc/crontab"
-# MYCRONVAR="32 4 * * 0 root sh /usr/local/src/mycron.sh"
-MYCRONVAR="* * * * * root sh /usr/local/src/mycron.sh"
+CRONY1="/etc/crontab"
+CRONY2="* * * * * root sh /usr/local/src/mycron.sh"
+MYCRONVAR="32 4 * * 0 root sh /usr/local/src/mycron.sh"
 
-if grep -q mycron "$CRONY"; then
+if grep -q "$CRONY2" "$CRONY1"; then
     # If "mycron" is present in crontab then select all lines in the crontab file that do NOT contain "mycron" and send them to a new file: tmpcrontab.
     # Then replaces crontab with tmpcrontab.
     grep -v "mycron" /etc/crontab > /usr/local/src/tmpcrontab && mv /usr/local/src/tmpcrontab /etc/crontab
