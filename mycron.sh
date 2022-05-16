f41827bfe377828e1899a7e83e880c741451f0ce
Michael Keenan <keenan@michaelkeenan.com>
2022-05-15 22:01:32 -0400

    Update mycron.sh

--- mycron.sh
+++ mycron.sh
@@ -4,12 +4,12 @@
 # 32 4 * * 0 root sh /usr/local/src/mycron.sh >> /etc/crontab
 
 CRONY="/etc/crontab"
-MYCRONVAR="32 4 * * 0 root sh /usr/local/src/mycron.sh"
+MYCRONVAR="32 4 \* \* 0 root sh /usr/local/src/mycron.sh"
 
 if grep -q mycron "$CRONY"; then
     # If "mycron" is present in crontab then select all lines in the crontab file that do NOT contain "mycron" and send them to a new file: tmpcrontab.
     # Then replaces crontab with tmpcrontab.
-    grep -v "mycron" /etc/crontab > /usr/local/src/tmpcrontab && mv /usr/local/src/tmpcrontab > /etc/crontab
+    grep -v "mycron" /etc/crontab > /usr/local/src/tmpcrontab && mv /usr/local/src/tmpcrontab /etc/crontab
     # Insert's new chrontab.sh line to crontab file.
     echo $MYCRONVAR >> /etc/crontab
 else
