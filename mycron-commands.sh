55e22e93b829a4b90b21dad335a9ef0a63317154
Michael Keenan <keenan@michaelkeenan.com>
2022-05-16 18:56:00 -0400

    Update mycron-commands.sh

--- mycron-commands.sh
+++ mycron-commands.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+# This will be ran by https://raw.githubusercontent.com/mdkeenan/linux/master/ocrap.sh every hour on applicable machines.
+# Can also be ran with: source <(curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/mycron-commands.sh)
+
+# Log the date and time the command is ran to the mycron.log.
+now=$(date)
+echo "mycron.sh ran at $now" >> /usr/local/src/mycron.log
