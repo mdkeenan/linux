c037ad6e19d519185268ec5437e9f423edd5fd52
Michael Keenan <keenan@michaelkeenan.com>
2022-05-15 23:40:00 -0400

    Update mycron.sh

--- mycron.sh
+++ mycron.sh
@@ -3,4 +3,6 @@
 # This will update from https://raw.githubusercontent.com/mdkeenan/linux/master/mycron.sh every Sundary at 04:32 and run the commands.
 # 32 4 * * 0 root sh /usr/local/src/mycron.sh >> /etc/crontab
 
+sudo apt-get update && sudo apt-get upgrade -y -qq > /dev/null
+
 curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/mycron.sh -o /usr/local/src/mycron.sh
