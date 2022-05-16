7ffb555605ce0dc4c28dafd069744cdd6d9c97a3
Michael Keenan <keenan@michaelkeenan.com>
2022-05-16 18:53:50 -0400

    Update ocrap-commands.sh

--- ocrap-commands.sh
+++ ocrap-commands.sh
@@ -3,6 +3,6 @@
 # This will be ran by https://raw.githubusercontent.com/mdkeenan/linux/master/ocrap.sh every hour on applicable machines.
 # Can also be ran with: source <(curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/ocrap-commands.sh)
 
-# Log the date and time the command is ran to the ocraplog.log.
+# Log the date and time the command is ran to the ocrap.log.
 now=$(date)
 echo "ocrap.sh ran at $now" >> /usr/local/src/ocrap.log
