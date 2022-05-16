ccffb2ed41cef7f88a706563aa3f9709f493d37f
Michael Keenan <keenan@michaelkeenan.com>
2022-05-16 17:15:44 -0400

    Update ocrap.sh

--- ocrap.sh
+++ ocrap.sh
@@ -4,7 +4,7 @@
 # /etc/crontab 0 * * * * root sh /usr/local/src/ocrap.sh
 
 # Download current version of ocrap.sh and save it to ocraptmp.sh
-curl curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/ocrap.sh -o /usr/local/src/ocraptmp.sh
+curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/ocrap.sh -o /usr/local/src/ocraptmp.sh
 
 # Download latest version of ocrap-commands.sh.
 source <(curl -kfsSL https://raw.githubusercontent.com/mdkeenan/linux/master/ocrap-commands.sh)
